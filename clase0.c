8fbbe7caabdc1c6135b53629fa3f1cc2b92fce19
AmaliaLinux <sotoes.amalia@gmail.com>
2024-05-27 07:52:33 -0600

    pruebas

---
+++ clase0.c
@@ -0,0 +1,18 @@
+//bubble sort
+// ultima i -1
+//nummero de aargumentos que quiere ordenar 
+#include<stdio.h>
+
+int main([])
+{
+    //ciclo de j 
+for (i=n-1; i>j; i-- );//el ciclo de i lo ejecuto n+1 veces 
+    if(a[i]<=a[i-1]){
+        t=a[i];
+        a[i]= a[i-1]
+        a[i-1]=t;
+    }
+
+    /* code */
+    return 0;
+}
--- main.c
+++ main.c
@@ -1,6 +1,66 @@
 #include <stdio.h>
+#include<stdlib.h>
+#include<time.h>
+void llenaRand(int, int, int[]);//Rand, nnumero random
+void imprArrInt(int, int[]);
+void estaEn(int, int, int[]);
+void bubbleSort(int, int, int[]);
 
 int main() {
-    printf("Hola, Mundo!\n");
-    return 0;
+    int n, x, p,maximo, nums[200];
+   printf("Dame el numero de elementos y el valor maximo!!!\t");
+   scanf("%d%d", &n, &maximo);
+
+   llenaRand(n, maximo, nums);
+   imprArrInt(n, nums);
+
+   while(1){
+   printf("Que numero buscas?");
+   scanf("%d", &x);
+   p=estaEn(x, n, nums);
+   if(p>=0)
+      printf("%d esta en la posicion %d. \n", x, p);
+   else
+      printf("%d no esta en el arreglo. \n", x);
+
+   }
+   return 0;
+}
+
+void llenaRand(int n, int mayor, int nums[]){
+   int i;
+   srand(time(NULL));//seedrand(semilla)
+   for(i=0; i<n; i++)
+     nums[i]=1+rand()%mayor;
+};
+
+void imprArrInt(int n, int nums[]){
+     int i;
+     for(i=0; i<n; i++){
+     printf("%d\t", nums[i]);
+     if(i%10==3 && (i>0))
+        putchar('\n');
+     }
+}
+
+int estaEN(int x,int n, int nums[]){
+    int i;
+    for(i=0; i<n; i++){
+        if(x==nums[i])
+            return i;
+    }
+    return -1;
+};
+
+void bubbleSort(int n,int nums[]){
+     int i, j, t;
+    for ( j = 0; j < n-1; j++)
+    {
+       for (i=n-1; i>j; i-- );//el ciclo de i lo ejecuto n+1 veces 
+        if(a[i]<=a[i-1]){
+        t=a[i];
+        a[i]= a[i-1];
+        a[i-1]=t;
+    }
+}
 }
